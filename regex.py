658eff2f2b2f57d76efc49fd2300438a1dd44644
Mykal Cuin <mykcuin@gmail.com>
2014-10-13 13:19:56 -0600

    Finished with regex.

--- regex.py
+++ regex.py
@@ -4,7 +4,7 @@ import re
 
 line = "Cats are smarter than dogs"
 
-matchObj = re.match(r'(.*) are (.?) .*', line, re.M|re.I)
+matchObj = re.match(r'(.*) are (.*?) .*', line, re.M|re.I)
 
 if matchObj:
 	print "matchObj.group(): ", matchObj.group()
@@ -12,3 +12,23 @@ if matchObj:
 	print "matchObj.group(2): ", matchObj.group(2)
 else:
 	print "No match!"
+
+if matchObj:
+	print "match --> matchObj.group(): ", matchObj.group()
+else:
+	print "No match!";
+
+searchObj = re.search(r'dogs', line, re.M|re.I)
+
+if searchObj:
+	print "search --> searchObj.group(): ", searchObj.group()
+else:
+	print "Nothing found!"
+
+phone = "2004-959-559 # This is phone number."
+
+num = re.sub(r'#.*$', "", phone)
+print "Phone number: ", num
+
+num = re.sub(r'\D', "", phone)
+print "Phone number: ", num
